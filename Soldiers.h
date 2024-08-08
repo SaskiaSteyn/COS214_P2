902a2319c19a866da26c4ce144262ced3622d251
JohanJvR <u22590732@tuks.co.za>
2024-08-08 11:18:22 +0200

    Updated Soldier files

--- Soldiers.h
+++ Soldiers.h
@@ -20,18 +20,21 @@ class Soldiers {
         void rest();
 
     public:
-        virtual Soldiers * clonis() = 0;
 
-    virtual void engage();
+        Soldiers(int health, int damage, int defence, int amt, string name);
+
+        virtual Soldiers * clonis() = 0;
+        virtual int getHealth() = 0;
+        virtual int getDamage() = 0;
+        virtual int getDefence() = 0;
+        virtual int getAmt() = 0;
+
+
+        virtual void engage();
         void disengage();
 
         Memento * militusMemento();
         void vivificaMemento(Memento * mem);
-
-    virtual int getHealth() = 0;
-    virtual int getDamage() = 0;
-    virtual int getDefence() = 0;
-    virtual int getAmt() = 0;
 };
 
 #endif //PRAC2_SOLDIERS_H
