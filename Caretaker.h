64768f135ac24cb65c9ce3e3141dd9fc049b4b12
JohanJvR <u22590732@tuks.co.za>
2024-08-09 13:19:57 +0200

    Mentos and Caretaker

--- Caretaker.h
+++ Caretaker.h
@@ -5,9 +5,20 @@
 #ifndef PRAC2_CARETAKER_H
 #define PRAC2_CARETAKER_H
 
+#include <iostream>
+#include <vector>
+#include "Memento.h"
+
+using namespace std;
 
 class Caretaker {
-
+    private:
+        vector<Memento> *storage;
+    public:
+        Caretaker();
+        Memento* getMemento(int index);
+        int getMementoIndex(Memento* memento);
+        void setMemento(Memento* memento, int value1, int value2, int value3, int value4, string value5);
 };
 
 
